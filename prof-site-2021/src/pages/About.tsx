3503f32ca825e63c08ec14d8f61f66c75f97418a
jsloboda <jsloboda@umich.edu>
2021-11-21 22:32:56 -0700

    checkpoint: About page div sections

--- prof-site-2021/src/pages/About.tsx
+++ prof-site-2021/src/pages/About.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Box from '@mui/material/Container';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
+import Divider from '@mui/material/Divider';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import List from '@mui/material/List';
@@ -30,9 +29,99 @@ class About extends React.Component {
                         </Grid>
                     </Box>
                 </Grid>
+
+                <Grid item xs={10} sx={{mb:6}}>
+                    <Divider textAlign="left" variant="fullWidth" sx={{width:'100%', mb:2}}>
+                        <Typography variant="h6">Broad algorithm development experience</Typography>
+                    </Divider>
+                    <Typography align="left" variant="body2" color="common.black">
+                        machine learning model development 
+                        (time series prediction/classificaiton, object detection & image classification,
+                        lanuage models) and high-level embedded deployment,
+                        physiological signal processing, speech analysis, 
+                        geospatial analysis, and social statistics.
+                    </Typography>
+                </Grid>
+                <Grid item xs={10} sx={{mb:4}}>
+                    <Divider textAlign="left" variant="fullWidth" sx={{width:'100%', mb:2}}>
+                        <Typography variant="h6">Supporting software development</Typography>
+                    </Divider>
+                    <Typography align="left" variant="body2" color="common.black" paragraph>
+                        Supporting software development for data preparation, exploratory analysis,
+                        scalable experimentation, visualization.     
+                    </Typography>
+                    <Typography align="left" variant="body2" color="common.black">
+                        Web development & design (~1 year experience).
+                    </Typography>
+                </Grid>
+                <Grid item xs={10} sx={{mb:4}}>
+                    <Divider textAlign="left" variant="fullWidth" sx={{width:'100%', mb:2}}>
+                        <Typography variant="h6">Collaboration & Communication</Typography>
+                    </Divider>
+                    <Typography align="left" variant="body2" color="common.black">
+                        Frequently work on interdisciplinary teams and 
+                        communicate to audiences with a range of technical backgrounds. 
+                        Engage with end users & domain experts. Create & present reports/briefings.     
+                    </Typography>
+                </Grid>
+
                 
                 <Grid item>
                 <Grid container justifyContent="center" sx={{mt: 4}}>
+                    <Grid item  xs={10} md={5} sx={{m: 2}}>
+                            <Typography align="center" variant="h6" paragraph color="common.black" fontStyle="bold">
+                                Broad algorithm development experience: 
+                            </Typography>
+                            <Typography align="center" variant="body2" color="common.black">
+                                <List dense={true}>
+                                    <ListItem>- machine learning model development</ListItem>
+                                    <ListItem sx={{pl:5}}>time series prediction & classificaiton, object detection & image classification,
+                                    lanuage models</ListItem>
+                                    <ListItem>- high-level embedded model deployment</ListItem>
+                                    <ListItem>- physiological signal processing</ListItem>
+                                    <ListItem>- speech analysis</ListItem>
+                                    <ListItem>- geospatial analysis</ListItem>
+                                    <ListItem>- social statistics</ListItem>
+                                </List>
+                            </Typography>
+                    </Grid>
+                    <Grid item  xs={10} md={5} sx={{m: 2}}>
+                            <Typography align="center" variant="h6" paragraph color="common.black" fontStyle="bold">
+                                Software development supporting:
+                            </Typography>
+                            <Typography align="center" variant="body2" color="common.black">
+                                <List dense={true}>
+                                    <ListItem>- data wrangling & preparation</ListItem>
+                                    <ListItem>- exploratory analysis</ListItem>
+                                    <ListItem>- scalable experimentation</ListItem>
+                                    <ListItem>- visualization</ListItem>
+                                </List>
+                            </Typography>
+                            <Typography align="center" variant="body1" color="common.black" fontStyle="bold">
+                                Web development & design
+                            </Typography>
+                            <Typography align="center" variant="body2" color="common.black" fontStyle="italic">
+                                (~1 year experience)
+                            </Typography>
+                    </Grid>
+                    <Grid item  xs={10} sx={{m: 2}}>
+                            <Typography align="center" variant="body1" color="common.black" fontStyle="bold">
+                                Engage with end users & domain experts. Create & present reports/briefings.
+                            </Typography>
+                    </Grid>
+                </Grid>
+                </Grid>
+            </Grid>   
+        )
+    }
+}
+
+export default About;
+
+/*
+    <Grid item>
+                <Grid container justifyContent="center" sx={{mt: 4}}>
+                    <Divider textAlign="left">LEFT</Divider>
                     <Grid item  xs={10} md={5} sx={{m: 2}}>
                         <Card sx={{p:5, backgroundColor: blueGrey[200], border:'0px solid black'}}>
                             <Typography align="center" variant="h6" paragraph color="common.black" fontStyle="bold">
@@ -84,19 +173,6 @@ class About extends React.Component {
                     </Grid>
                 </Grid>
                 </Grid>
-            </Grid>   
-        )
-    }
-}
-
-export default About;
-
-/*
-                <Grid item sx={{p: 3, mt: 3}}>
-                    <Typography align="center" variant="subtitle1">
-                        Developing a "full stack data science" skillset.
-                    </Typography>
-                </Grid> 
 */
 
 /*
