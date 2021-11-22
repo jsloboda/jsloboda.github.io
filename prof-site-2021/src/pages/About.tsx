bb1bef2d88211ea45425d0fced5ef56347d059ca
jsloboda <jsloboda@umich.edu>
2021-11-21 17:12:47 -0700

    box styling checkpoint

--- prof-site-2021/src/pages/About.tsx
+++ prof-site-2021/src/pages/About.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import Box from '@mui/material/Container';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
 import Typography from '@mui/material/Typography';
+import { blueGrey } from '@mui/material/colors';
 
 class About extends React.Component {
     render() {
@@ -25,14 +28,63 @@ class About extends React.Component {
                         </Grid>
                     </Box>
                 </Grid>
-                <Grid item xs={12} sx={{mb: 7}}>
-                    <Grid container justifyContent='center'>
-                        <img src='wfh_zion_icon2-38t-onwhite.png' width='300'></img>
+                
+                <Grid item>
+                <Grid container justifyContent="center" sx={{mt: 4}}>
+                    <Grid item  xs={10} md={5} sx={{m: 2}}>
+                        <Box sx={{p:5, backgroundColor: blueGrey[300], border:'0px solid black'}}>
+                            <Typography align="center" variant="h6" paragraph color="common.black" fontStyle="bold">
+                                Broad algorithm development experience: 
+                            </Typography>
+                            <Typography align="center" variant="body2" color="common.black">
+                                <List dense={true}>
+                                    <ListItem>- machine learning model development</ListItem>
+                                    <ListItem>(time series prediction/classificaiton, object detection & image classification,
+                                    lanuage models)</ListItem>
+                                    <ListItem>- high-level embedded model deployment</ListItem>
+                                    <ListItem>- physiological signal processing</ListItem>
+                                    <ListItem>- speech analysis</ListItem>
+                                    <ListItem>- geospatial analysis</ListItem>
+                                    <ListItem>- social statistics</ListItem>
+                                </List>
+                            </Typography>
+                        </Box>
+                    </Grid>
+                    <Grid item  xs={10} md={5} sx={{m: 2}}>
+                        <Box sx={{p:5, backgroundColor: blueGrey[300], border:'0px solid black'}}>
+                            <Typography align="center" variant="h6" paragraph color="common.black" fontStyle="bold">
+                                Software development supporting:
+                            </Typography>
+                            <Typography align="center" variant="body2" color="common.black">
+                                <List dense={true}>
+                                    <ListItem>- data wrangling & preparation</ListItem>
+                                    <ListItem>- exploratory analysis</ListItem>
+                                    <ListItem>- scalable experimentation</ListItem>
+                                    <ListItem>- visualization</ListItem>
+                                    <ListItem>- web development & design (1 year)</ListItem>
+                                </List>
+                            </Typography>
+                        </Box>
                     </Grid>
                 </Grid>
+                </Grid>
+            </Grid>   
+        )
+    }
+}
 
+export default About;
 
-                <Grid item minWidth='100%'>
+/*
+                <Grid item sx={{p: 3, mt: 3}}>
+                    <Typography align="center" variant="subtitle1">
+                        Developing a "full stack data science" skillset.
+                    </Typography>
+                </Grid> 
+*/
+
+/*
+<Grid item minWidth='100%'>
                 <Box sx={{p: 5, mt: 4, backgroundColor: 'lightgray', minWidth:'100%'}}>
                     <Grid item>
                         <Typography align="center" variant="body2" paragraph>
@@ -46,7 +98,7 @@ class About extends React.Component {
                         <Typography align="center" variant="body2" paragraph>
                             Supporting software development for 
                             data preparation, exploratory analysis, scalable experimentation, visualization. 
-                            Aimed at 
+                            Developing a "full stack data science" skillset.
                         </Typography>
                         <Typography align="center" variant="body2" paragraph>
                             Engage with end users & domain experts. Create & present reports/briefings.
@@ -68,17 +120,4 @@ class About extends React.Component {
                     </Grid>
                 </Box>
                 </Grid>
-            </Grid>   
-        )
-    }
-}
-
-export default About;
-
-/*
-                <Grid item sx={{p: 3, mt: 3}}>
-                    <Typography align="center" variant="subtitle1">
-                        Developing a "full stack data science" skillset.
-                    </Typography>
-                </Grid> 
 */
